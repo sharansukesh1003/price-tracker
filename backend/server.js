db91ce7bdcd4361efb9f4479c75171cec064a57c
sharansukesh1003 <sharansukesh2000@gmail.com>
2021-11-18 01:20:26 +0530

    fetch fix pending

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,21 @@ const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
 const { request, response } = require('express')
+const cors = require("cors")
+
+const corsOptions = {
+    origin : '*', 
+    credentials : true,          
+    optionSuccessStatus : 200,
+ } 
 
 const app = express()
+app.use(cors(corsOptions))
 app.use(express.json())
 
 app.post('/', async (req, res) => {
-    productLink = req.body.prod_link
+    productLink = req.body.url
+    useragent = req.body.userAgent
     axios.post('http://127.0.0.1:8080/amazon/', {
         prod_link : productLink
     }).then((response) => {
@@ -23,7 +32,7 @@ app.post('/', async (req, res) => {
                 })  
             }
             else{
-                res.send({
+                res.json({
                     code: 200,
                     product_title: productTitle,
                     product_price: productPrice,
--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -1,14 +1,21 @@
-async function sendRequest() {
-  let url = document.getElementById("urlField").value
-  let userAgent = document.getElementById("userAgentField").value
-  const data = { url, userAgent }
-  const options = {
-    method: 'POST', 
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body : JSON.stringify(data)
-  }
-  const response = await fetch('http://127.0.0.1:8000/', options)
+window.onload = function () {
+  document.getElementById("submit-data").onclick = async () => {
+    let url = document.getElementById("urlField").value
+    let userAgent = document.getElementById("userAgentField").value
+    const data = {
+      url,
+      userAgent
+    }
+    const options = {
+      method: 'POST',
+      mode: 'no-cors',
+      headers: {
+        'Access-Control-Allow-Origin' : '*',
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }
+    fetch('http://127.0.0.1:8000/', options)
+  };
 }
-  
